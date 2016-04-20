4ddf6db3d5b61a4641ece9a5316b7abedf0f5ff7
Łukasz Górecki <36896@KTIPM-025.ue.poznan>
2016-04-20 18:59:23 +0200

    Last edit

--- ProjectManagement/src/main/App.java
+++ ProjectManagement/src/main/App.java
@@ -8,6 +8,7 @@ public class App {
 		System.out.println("Na górze ró¿e");
 		System.out.println("Na dole lodówka");
 		System.out.println("Ten wiersz jest bez sensu");
+		System.out.println("Mikrofalówka");
 		
 	}
 
