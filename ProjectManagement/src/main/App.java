8ce32b4a4571095f2b746d5c1450756ea35c7ca2
Tomasz Hinc <30206@KTIPM-008.ue.poznan>
2016-04-20 18:57:15 +0200

    Poem line added

--- ProjectManagement/src/main/App.java
+++ ProjectManagement/src/main/App.java
@@ -7,6 +7,7 @@ public class App {
 		System.out.println("Oto wiersz:");
 		System.out.println("Na górze ró¿e");
 		System.out.println("Na dole lodówka");
+		System.out.println("Ten wiersz jest bez sensu");
 		
 	}
 
